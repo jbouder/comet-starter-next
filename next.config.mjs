5aca5693b66d16b438c9aab4588749c99a8a2b24
Johnny Bouder <jbouder@metrostar.com>
2024-01-24 21:06:00 -0500

    Add uswds support to project.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import path from "path";
+import * as sass from "sass";
+
+const basePath = process.env.BASE_PATH || "";
+const nextConfig = {
+  basePath,
+  reactStrictMode: true,
+  sassOptions: {
+    includePaths: [
+      "./node_modules/@uswds",
+      "./node_modules/@uswds/uswds/packages",
+    ],
+    functions: {
+      "add-base-path($path)": (path) => {
+        return new sass.SassString(`${basePath}${path.getValue()}`);
+      },
+    },
+  },
+};
 
 export default nextConfig;
