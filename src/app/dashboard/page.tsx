10e9391953c99f012bef8e396983d050a88c3524
Johnny Bouder <jbouder@metrostar.com>
2024-07-09 08:22:11 -0400

    Add base dashboard page to finish signin flow.

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+export default function Dashboard() {
+  return (
+    <div className="grid-container">
+      <div className="grid-row">
+        <div className="tablet:grid-col-6">
+          <h1>Dashboard</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
