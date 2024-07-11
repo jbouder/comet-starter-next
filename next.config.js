c08387d5b4e2d02cdf64ccb6ea13b19a239ec80e
Johnny Bouder <jbouder@metrostar.com>
2024-07-11 19:47:13 -0400

    Fix build.

--- next.config.js
+++ next.config.js
@@ -1,8 +1,23 @@
-module.exports = {
+const nextConfig = {
   sassOptions: {
     includePaths: [
       "./node_modules/@uswds",
       "./node_modules/@uswds/uswds/packages",
     ],
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.(svg)$/,
+      use: [
+        {
+          loader: "svg-url-loader",
+          options: { limit: 10000 },
+        },
+      ],
+    });
+
+    return config;
+  },
 };
+
+module.exports = nextConfig;
