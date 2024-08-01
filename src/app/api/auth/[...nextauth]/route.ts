54487392a6121996549fcb039f517a824c959a44
Brian Cash <bcash2077@gmail.com>
2024-08-01 17:58:07 +0000

    Adding keycloak sso button.

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,17 @@
+import NextAuth from 'next-auth';
+import KeycloakProvider from 'next-auth/providers/keycloak'
+
+
+export const authOptions = {
+    providers:[
+        KeycloakProvider({
+            clientId: process.env.KEYCLOAK_CLIENT_ID,
+            clientSecret: process.env.KEYCLOAK_CLIENT_SECRET,
+            issuer: process.env.KEYCLOAK_ISSUER
+        })
+    ],
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST}
