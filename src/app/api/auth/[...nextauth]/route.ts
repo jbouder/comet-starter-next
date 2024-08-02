447140475c7b5ebab6986053c5a3c130aa1cad3e
Johnny Bouder <jbouder@metrostar.com>
2024-08-02 10:44:52 -0400

    Fix build errors.

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,17 +1,16 @@
-import NextAuth from 'next-auth';
-import KeycloakProvider from 'next-auth/providers/keycloak'
+import NextAuth from "next-auth";
+import KeycloakProvider from "next-auth/providers/keycloak";
 
-
-export const authOptions = {
-    providers:[
-        KeycloakProvider({
-            clientId: process.env.KEYCLOAK_CLIENT_ID,
-            clientSecret: process.env.KEYCLOAK_CLIENT_SECRET,
-            issuer: process.env.KEYCLOAK_ISSUER
-        })
-    ],
+const authOptions = {
+  providers: [
+    KeycloakProvider({
+      clientId: process.env.KEYCLOAK_CLIENT_ID ?? "",
+      clientSecret: process.env.KEYCLOAK_CLIENT_SECRET ?? "",
+      issuer: process.env.KEYCLOAK_ISSUER ?? "",
+    }),
+  ],
 };
 
 const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST}
+export { handler as GET, handler as POST };
